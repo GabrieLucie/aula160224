e4dafd5034c0a0a6d5302ede8d7aba792edb4dd2
GabrieLucie <gabriellucie06@gmail.com>
2024-02-23 12:25:05 +0000

    aula2302

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,11 +1,35 @@
 package primeiro_exemplo;
-
+ 
+import primeiro_exemplo.modelos.Pessoa;
+ 
 public class Hello {
     public static void main(String[] args) {
         int cont = 10;
         String nome = "Marco";
         System.out.println("Olá Mundo!!!");
-
+ 
         System.out.println(cont + " : " + nome);
+ 
+        Pessoa p1 = new Pessoa();
+ 
+        p1.setNome = ("Gabriel");
+        p1.setIdade = (17);
+ 
+        System.out.println(p1.getNome() + " : " + p1.getIdade());
+ 
+        Pessoa p2 = p1;
+ 
+        p2.nome = "Munhoz";
+       
+        System.out.println(p2.nome);
+        System.out.println(p1.nome);
+ 
+        Pessoa p3 = new Pessoa();
+        p3.nome = "Clodoaldo";
+        p3.idade = 30;
+ 
+        System.out.println(p1.verificarMarioridade());
+        System.out.println(p3.verificarMarioridade());
+ 
     }
 }
